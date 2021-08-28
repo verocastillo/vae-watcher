04357affaabc79d4d2a8ee8a910c45212535875d
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-08-27 20:29:03 -0500

    Testing js app.

--- static/js/app1.js
+++ static/js/app1.js
@@ -1,8 +1,28 @@
-// Import JSON file
-fetch("https://vae-project.herokuapp.com/api")
-.then(response => {
-   return response.json();
-})
-.then(data => console.log(data));
+// Function to build the page
+function buildPage() {
 
-console.log(data.top10symptoms)
+// Function for unique values (https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates)
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+// Get data from API
+const url = "/api";
+d3.json(url).then(function(response) {
+  const data = response;
+
+  // Get data to use
+  const as_all = data.agesymptoms-all;
+  const as_pfz = data.agesymptoms-pfizer;
+  const as_mod = data.agesymptoms-moderna;
+  const top10 = data.top10symptoms;
+  const totvax = data.totalvaccines;
+
+  // Get top 10
+  document.getElementById("card-text3").innerHTML = "New text!"
+
+});
+}
+
+
+buildPage();
