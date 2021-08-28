557883dacfefe35e5f7c41a101f8f18e72b17b7e
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-08-27 21:19:20 -0500

    Finished vaccines page.

--- static/js/app1.js
+++ static/js/app1.js
@@ -1,19 +1,21 @@
 // Function to build the page
 function buildPage() {
 
-// Function for unique values (https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates)
-function onlyUnique(value, index, self) {
-  return self.indexOf(value) === index;
-}
-
 // Get data from API
 const url = "/api";
 d3.json(url).then(function(response) {
   const data = response;
-  console.log(data[0])
   // Get top 10
   d3.select("#p1").each(function(d, i) {
     d3.select(this).text("From 362,670,106 COVID vaccines applied, only 24417 VAEs were reported");
+  });
+  d3.select("#p2").each(function(d, i) {
+    var symptom1 = data.SYMPTOM1
+    mode = d3.mode(symptom1)
+    d3.select(this).text("The most common symptoms are headache, chills and swelling in injection site, which are not severe");
+  });
+  d3.select("#p3").each(function(d, i) {
+    d3.select(this).text("Younger women reported more side effects, while, in contrast, older men did.");
   });
 });
 }
