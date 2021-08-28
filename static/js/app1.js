9beb4b5c656263fdb4cccb62706fa6ef4e0ff597
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-08-27 19:44:51 -0500

    Checking with API.

--- static/js/app1.js
+++ static/js/app1.js
@@ -1,4 +1,8 @@
-// Import dataset from data.json
-var tableData = data;
+// Import JSON file
+fetch("https://vae-watcher.herokuapp.com/api")
+.then(response => {
+   return response.json();
+})
+.then(data => console.log(data));
 
-console.log(tableData)
+console.log(data.top10symptoms)
